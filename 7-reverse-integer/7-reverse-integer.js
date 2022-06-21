5f200412e7d5b35a2f861b6fabdd03f8241dfe8b
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2022-06-21 22:59:45 +0900

    Time: 126 ms (22.27%), Space: 44 MB (45.42%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    const max = Math.pow(2, 31) - 1;
+    const min = Math.pow(-2, 31);
+    let reverseInt = parseInt(x.toString().split('').reverse().join(''));
+    if (x < 0) {
+        reverseInt = reverseInt * -1;
+    }
+    return min <= reverseInt && reverseInt <= max ? reverseInt : 0;
+};
