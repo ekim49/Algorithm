16144a0fe7ae703c08e0ffe5ed5c6c634236d9db
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2022-07-12 16:28:40 +0900

    Time: 75 ms (79.00%), Space: 42.3 MB (39.33%) - LeetHub

--- 50-powx-n/50-powx-n.js
+++ 50-powx-n/50-powx-n.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    return x**n;
+    // return x**n;
+    
+    // recursion
+    // base case
+    if (n === 0) return 1;
+    
+    // recursive
+    if (n < 0) return 1 / myPow(x, -n);
+    if (n % 2 === 1) return x * myPow(x, n - 1);
+    
+    return myPow(x * x, n / 2);
 };
