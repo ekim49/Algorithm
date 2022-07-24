98dd96f4aad9f1b6565c4c2b3ba818d7f273ff8c
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2022-07-24 18:21:54 +0900

    Time: 219 ms (42.30%), Space: 63.8 MB (56.20%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (obj[nums[i]] >= 0 && i - obj[nums[i]] <= k) return true;
+        obj[nums[i]] = i;
+    }
+    return false;
+};
+
