4c39af8bbaf57f43144d444c2bf25a973fa5cb48
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2022-08-16 23:22:22 +0900

    Time: 92 ms (34.25%), Space: 41.9 MB (56.91%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function(sentence, searchWord) {
+    const arr = sentence.split(' ');
+    
+    for (let i = 0; i < arr.length; i++) {    
+        if (arr[i].slice(0, searchWord.length) === searchWord) {
+            return i + 1;
+        }
+    }
+    return -1;
+};
