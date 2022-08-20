2c7d1649bbcf21eda2eccd536fadf72ddbd11361
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2022-08-20 20:00:23 +0900

    Time: 113 ms (32.02%), Space: 42.9 MB (56.16%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function(s) {
+    let count = 1;
+    let max = 1;
+    let countArr = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i + 1]) {
+            count ++;
+            max = Math.max(count, max)
+        } else {
+            count = 1
+        }
+    }
+    return max;
+};
