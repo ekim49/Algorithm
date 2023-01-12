4ab5837fb11bd23bbcc40b4a80d98de4a4208a24
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2023-01-12 11:39:07 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,5 +1,4 @@
 function solution(s) {
-    const regex = /^-?\d+$/;
-    if (s.length === 4 || s.length === 6) return regex.test(s);
-    return false;
+    const regex = /^\d{6}$|^\d{4}$/;
+	return regex.test(s);
 }
