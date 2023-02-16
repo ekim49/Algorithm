1cac44238236b45120ecc261eda063a2036b81bd
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2023-02-16 11:36:35 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,5 @@
 function solution(s) {
-    const numbers = s.split(' ').map((el) => Number(el)).sort((a, b) => a - b);
-    const smallest = numbers[0];
-    const largest = numbers[numbers.length - 1];
-    return `${smallest} ${largest}`;
+    const numbers = s.split(' ');
+    
+    return `${Math.min(...numbers)} ${Math.max(...numbers)}`;
 }
