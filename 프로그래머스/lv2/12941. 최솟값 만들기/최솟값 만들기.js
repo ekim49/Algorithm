843f7335b06a16ab82c5ce728a4833c113ffd2e5
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2023-02-25 11:47:59 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.71 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,6 +1,7 @@
-function solution(A, B) {
-  A.sort((a, b) => a - b);
-  B.sort((a, b) => b - a);
+function solution(A,B){
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
     
-  return A.map((a, i) => a * B[i]).reduce((sum, val) => sum + val, 0);
+    // reduce 함수의 세 번째 인자 index를 활용해서 풀이
+    return A.reduce((total, val, idx) => total + val * B[idx], 0)
 }
