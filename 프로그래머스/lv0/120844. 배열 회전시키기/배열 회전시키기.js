857eae9e75317d329697ddc40c47534525b495e8
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2023-04-13 15:49:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,10 +1,12 @@
 function solution(numbers, direction) {
-    if (direction === 'right') {
-        let 이동할숫자 = numbers.pop();
-        numbers.unshift(이동할숫자);
-    } else {
-        let 이동할숫자 = numbers.shift();
-        numbers.push(이동할숫자);
-    }
+    // if (direction === 'right') {
+    //     let 이동할숫자 = numbers.pop();
+    //     numbers.unshift(이동할숫자);
+    // } else {
+    //     let 이동할숫자 = numbers.shift();
+    //     numbers.push(이동할숫자);
+    // }
+    // return numbers;
+    direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
     return numbers;
 }
