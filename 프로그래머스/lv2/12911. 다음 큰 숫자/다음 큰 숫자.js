a5f399574fe87355282ae9395a4dde6d90c9c738
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2023-04-14 17:38:45 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    // 1. n을 2진수로 변환해서 1의 개수를 count
+    // 2. 반복문으로 n++ 하면서 2진수로 변환
+    // 3. 변환된 2진수의 1개수를 count해서 1번과 같으면 원래 숫자 리턴
+    let n의이진수 = n.toString(2);
+    let n의이진수의1개수 = (n의이진수.match(/1/g) || []).length;
+    
+    for (let i = n + 1; i < n * 2; i++) {
+        let binary = i.toString(2);
+        let oneCnt = (binary.match(/1/g) || []).length;
+        
+        if (n의이진수의1개수 === oneCnt) return i;
+    }
+}
