a59331a5244b9bc638a90aa524bf38ac7b50f82a
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2023-04-19 11:00:48 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.92 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,8 @@
 function solution(arr) {
-    let unique = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[i + 1]) unique.push(arr[i]);
-    }
-    return unique;
+    // let unique = [];
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] !== arr[i + 1]) unique.push(arr[i]);
+    // }
+    // return unique;
+    return arr.filter((el, idx) => el !== arr[idx + 1]);
 }
