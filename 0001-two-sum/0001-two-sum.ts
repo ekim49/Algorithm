8dd6fba4e128a87eb5aff69036c695bc16da2451
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2025-07-23 21:52:09 +0900

    Time: 4 ms (56.95%), Space: 57.1 MB (40.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,18 +1,10 @@
 function twoSum(nums: number[], target: number): number[] {
-    // for (let i = 0; i < nums.length; i++) {
-    //     const numToFind = target - nums[i]
-    //     const j = nums.indexOf(numToFind);
-    //     if (j !== -1 && i !== j) return [i, j];
-    // }
-
-    // hash map - O(n)
-    const map = new Map<number, number>();
+    // hash map O(1)
+    const map = new Map();
 
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-
-        if (map.has(complement)) return [map.get(complement), i];
-
+        const diff = target - nums[i];
+        if (map.has(diff)) return [map.get(diff), i];
         map.set(nums[i], i);
     }
 };
