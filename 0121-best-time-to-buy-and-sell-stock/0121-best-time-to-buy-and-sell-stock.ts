8a01d9b49eed562dd9aa23ecf1a8540b40daf8cf
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2025-07-31 22:16:13 +0900

    Time: 4 ms (33.61%), Space: 66.7 MB (15.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,17 +1,31 @@
 function maxProfit(prices: number[]): number {
+    // sliding window
+    // let buy = 0;
+    // let sell = 1;
+    // let maxProfit = 0;
+
+    // while (sell < prices.length) {
+    //     if (prices[buy] < prices[sell]) {
+    //         const profit = prices[sell] - prices[buy];
+    //         maxProfit = Math.max(maxProfit, profit)
+    //     } else {
+    //         buy = sell;
+    //     }
+    //     sell += 1;
+    // }    
+    // return maxProfit;
+
     let minPrice = Infinity;
     let maxProfit = 0;
 
-    for (let i = 0; i < prices.length; i++) {
-        const price = prices[i];
-        const profit = price - minPrice;
+    for (let price of prices) {
         if (price < minPrice) {
-            minPrice = price;
-        } else {
-            if (profit > maxProfit) {
-                maxProfit = profit;
-            }
+            minPrice = price; // update minPrice if price is smaller
+        } else { // price >= minPrice -> time to sell?
+            const profit = price - minPrice;
+            maxProfit = Math.max(profit, maxProfit);
         }
     }
+
     return maxProfit;
 };
