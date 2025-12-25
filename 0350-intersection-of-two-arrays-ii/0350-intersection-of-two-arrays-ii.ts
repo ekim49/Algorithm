6d4c61c33ae4e2c22dea3a07d7f0668d87338af7
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2025-12-25 18:36:49 +0900

    Time: 3 ms (46.58%), Space: 58.8 MB (14.52%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.ts
@@ -0,0 +1,17 @@
+function intersect(nums1: number[], nums2: number[]): number[] {
+    const map = new Map();
+    const result = [];
+
+    for (let num of nums1) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+
+    for (let num of nums2) {
+        if (map.has(num) && map.get(num) > 0) {
+            result.push(num);
+            map.set(num, map.get(num) - 1);
+        }
+    }
+
+    return result;
+};
