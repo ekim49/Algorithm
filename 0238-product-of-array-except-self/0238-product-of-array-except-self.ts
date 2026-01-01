84404ae3f4cf721ff8fd8fcbca7b03bd2b29a6ce
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2026-01-01 22:56:59 +0900

    Time: 14 ms (27.26%), Space: 70.2 MB (64.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,22 +1,44 @@
 function productExceptSelf(nums: number[]): number[] {
+    // const n = nums.length;
+    // const left = Array(n).fill(1);
+    // const right = Array(n).fill(1);
+    // const result = Array(n);
+
+    // // from left
+    // for (let i = 1; i < n; i++) {
+    //     left[i] = left[i - 1] * nums[i - 1];
+    // }
+
+    // // from right
+    // for (let i = n - 2; i >= 0; i--) {
+    //     right[i] = right[i + 1] * nums[i + 1];
+    // }
+
+    // // left * right
+    // for (let i = 0; i < n; i++) {
+    //     result[i] = left[i] * right[i];
+    // }
+
+    // return result;
+
+    // Prefix & Suffix O(n)
     const n = nums.length;
-    const left = Array(n).fill(1);
-    const right = Array(n).fill(1);
-    const result = Array(n);
 
-    // from left
+    // products of everything before i
+    const prefix = new Array(n).fill(1);
     for (let i = 1; i < n; i++) {
-        left[i] = left[i - 1] * nums[i - 1];
+        prefix[i] = prefix[i - 1] * nums[i - 1];
     }
 
-    // from right
+    // products of everything after i
+    const suffix = new Array(n).fill(1);
     for (let i = n - 2; i >= 0; i--) {
-        right[i] = right[i + 1] * nums[i + 1];
+        suffix[i] = suffix[i + 1] * nums[i + 1];
     }
 
-    // left * right
+    const result = new Array(n);
     for (let i = 0; i < n; i++) {
-        result[i] = left[i] * right[i];
+        result[i] = prefix[i] * suffix[i];
     }
 
     return result;
