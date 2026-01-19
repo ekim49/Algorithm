1249c9f5e0a06f8599ed496bc50cfc1b23cd05ab
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2026-01-19 22:03:31 +0900

    Time: 1 ms (90.36%), Space: 66.2 MB (76.31%) - LeetHub

--- 0134-gas-station/0134-gas-station.ts
+++ 0134-gas-station/0134-gas-station.ts
@@ -1,12 +1,9 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-    // Greedy 
-    // time O(n) | space O(1)
-    // total sum of gas < total sum of cost -> return -1
-
+    // greedy - time O(n), space O(1)
     let totalGas = 0;
     let totalCost = 0;
-    let startIdx = 0;
     let tank = 0;
+    let start = 0;
 
     for (let i = 0; i < gas.length; i++) {
         totalGas += gas[i];
@@ -14,12 +11,11 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
 
         tank += gas[i] - cost[i];
 
-        // move the startIdx, initialize tank
         if (tank < 0) {
-            startIdx = i + 1;
+            start = i + 1;
             tank = 0;
         }
     }
 
-    return totalGas >= totalCost ? startIdx : -1;
+    return totalGas >= totalCost ? start : -1;
 };
