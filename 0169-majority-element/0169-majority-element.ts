d56fc3ac23f5d41ae6e277ddb61bafb0c6033701
Hailie Eunjoo Kim <100138593+ekim49@users.noreply.github.com>
2026-01-25 22:20:18 +0900

    Time: 2 ms (73.22%), Space: 59.8 MB (35.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,11 +1,27 @@
 function majorityElement(nums: number[]): number {
-    const map = new Map<number, number>();
-    const majority = nums.length / 2;
+    // 1. Hash Map (Time O(n), Space O(n))
+    // const map = new Map<number, number>();
+    // const majority = nums.length / 2;
 
-    for (const num of nums) {
-        const count = (map.get(num) ?? 0) + 1;
-        map.set(num, count);
+    // for (const num of nums) {
+    //     const count = (map.get(num) ?? 0) + 1;
+    //     map.set(num, count);
 
-        if (count > majority) return num;
+    //     if (count > majority) return num;
+    // }
+
+    // 2. Boyer-Moore (Time O(n), Space O(1))
+    let candidate = null;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+            count = 1;
+        } 
+        else if (num === candidate) count++;
+        else count--;
     }
+
+    return candidate;
 };
